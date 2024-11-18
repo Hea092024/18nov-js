713fad7e907e3052cf6245b91a7d6a25e541c222
Hea092024 <Hea092024@gmail.com>
2024-11-18 10:02:38 +0100

    .

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const clear = document.querySelector("#clear-array");
 const input = document.querySelector("#input");
 const listHeader = document.querySelector("#list-header");
 const theList = [];
+
+function addToArray(){
+    theList.push(input.value);
+    console.log(theList);
+}
+
+function arrayToHTML(){
+    for (let i =0; i< theList.lenght; i++){
+        const listItem = theList[i];
+        let child = document.createElement("li")
+        child.textContent = listItem
+    listHeader.appendChild((child))    }
+}
+
+
+toArray.addEventListener("click", addToArray);
